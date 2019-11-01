85e604ee4f9e4978502ce7c624e72470704568b7
Holger H <holgheiss@gmail.com>
2019-11-01 15:21:28 +0100

    try tweaking url player

--- components/URLPlayer.js
+++ components/URLPlayer.js
@@ -14,6 +14,15 @@ class URLPlayer extends PureComponent {
     return <WebView
       source={{ uri: this.props.url }}
       style={{ width, height }}
+      allowsInlineMediaPlayback
+      /* originWhitelist={['https://', 'http://', 'ws://', 'wss://']} */
+      /* user agent: desktop safari (to play minecraft) */
+      userAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15"
+      mixedContentMode="always" /* allow https to load http */
+      mediaPlaybackRequiresUserAction={false}
+      keyboardDisplayRequiresUserAction={false}
+      /* incognito */
+      cacheEnabled={false}
     />
   }
 }
